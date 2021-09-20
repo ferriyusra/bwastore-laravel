932254765fa2bfc20b5354191ac7dffa8f02685a
ferri yusra <ferisakuragi@gmail.com>
2021-09-20 15:51:39 +0700

    finishing login and register style custom to blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,46 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+
+ <!-- start section login -->
+ <div class="page-content page-auth">
+    <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+            <div class="row align-items-center row-login">
+                <div class="col-lg-6 text-center">
+                    <img src="{{ url('/images/login-placeholder.png')}}" class="w-50 mb-4 mb-lg-none">
+                </div>
+                <div class="col-lg-5">
+                    <h2>
+                        Belanja kebutuhan utama,
+                        <br>
+                        menjadi lebih mudah
+                    </h2>
+                    <form action="" class="mt-3">
+                        <div class="form-group">
+                            <label>Alamat Email</label>
+                            <input type="email" class="form-control w-75">
+                        </div>
+                        <div class="form-group">
+                            <label>Kata Sandi</label>
+                            <input type="password" class="form-control w-75">
+                        </div>
+                        <button class="btn btn-success btn-block w-75 mt-4">
+                            Masuk Akun
+                        </button>
+                        <button class="btn btn-signup btn-block w-75 mt-4">
+                            Registrasi Akun
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end section login -->
+
+
+<div class="container" style="display: none">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,76 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+<!-- start section login -->
+<div class="page-content page-auth" id="register">
+    <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+            <div class="row align-items-center row-login justify-content-center">
+                <div class="col-lg-4">
+                    <h2>
+                        Memulai untuk jual beli
+                        <br>
+                        dengan cara terbaru
+                    </h2>
+                    <form class="mt-3">
+                        <div class="form-group">
+                            <label>Nama Lengkap</label>
+                            <input type="text" class="form-control is-valid" v-model="name" autofocus>
+                        </div>
+                        <div class="form-group">
+                            <label>Alamat Email</label>
+                            <input type="email" class="form-control is-invalid" v-model="email">
+                        </div>
+                        <div class="form-group">
+                            <label>Kata Sandi</label>
+                            <input type="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Toko</label>
+                            <p class="text-muted">
+                                Apakah anda ingin membuka toko ?
+                            </p>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input type="radio" class="custom-control-input" name="is_store_open"
+                                    id="openStoreTrue" v-model="is_store_open" :value="true">
+                                <label for="openStoreTrue" class="custom-control-label">
+                                    Iya, Boleh
+                                </label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input type="radio" class="custom-control-input" name="is_store_open"
+                                    id="openStoreFalse" v-model="is_store_open" :value="false">
+                                <label for="openStoreFalse" class="custom-control-label">
+                                    Enggak dulu
+                                </label>
+                            </div>
+                        </div>
+                        <div class="form-group" v-if="is_store_open">
+                            <label>Nama Toko</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group" v-if="is_store_open">
+                            <label>Kategori</label>
+                            <select name="category" class="form-control">
+                                <option value="" disabled>Kategori Toko</option>
+                            </select>
+                        </div>
+                        <button class="btn btn-success btn-block mt-4">
+                            Registrasi Sekarang
+                        </button>
+                        <button class="btn btn-signup btn-block mt-4">
+                            Masuk Akun
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end section login -->
+
+
+<div class="container" style="display: none">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -75,3 +144,31 @@
     </div>
 </div>
 @endsection
+
+@push('addon-script')
+<script src="vendor/vue/vue.js"></script>
+<script src="https:unpkg.com/vue-toasted"></script>
+<script>
+    Vue.use(Toasted);
+    let register = new Vue({
+        el: "#register",
+        mounted() {
+            AOS.init();
+            this.$toasted.error(
+                "Maaf, email yang dimasukkan sudah terdaftar didalam sistem kami.", {
+                position: "top-center",
+                className: "rounded",
+                duration: "1000"
+            }
+            );
+        },
+        data: {
+            name: "ferri yusra aa",
+            email: "feri@gmail.com",
+            password: "",
+            is_store_open: true,
+            store_name: ""
+        }
+    });
+</script>
+@endpush
