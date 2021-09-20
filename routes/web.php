e99c5927b8a63d0807e68e80acdc75ad3cbc4aeb
ferri yusra <ferisakuragi@gmail.com>
2021-09-20 15:30:24 +0700

    finishing cart blade

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/categories', 'CategoryController@index')->name('categories');
 Route::get('/details/{id}', 'DetailController@index')->name('detail');
+Route::get('/cart', 'CartController@index')->name('cart');
 
 Auth::routes();
