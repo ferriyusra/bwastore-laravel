964f6c1f8b58267db75e4616ff14eea1fff4caf3
ferri yusra <ferisakuragi@gmail.com>
2021-09-20 14:49:30 +0700

    finishing category blade

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top" data-aos="fade-down">
     <div class="container">
-      <a href="index/html" class="navbar-brand">
+      <a href="{{ route('home')}}" class="navbar-brand">
         <img src="images/logo.svg" alt="Logo">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
@@ -8,13 +8,13 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a href="index.html" class="nav-link">
+          <li class="nav-item">
+            <a href="{{ route('home')}}" class="nav-link">
               Home
             </a>
           </li>
           <li class="nav-item">
-            <a href="categories.html" class="nav-link">
+            <a href="{{ route('categories')}}" class="nav-link">
               Categories
             </a>
           </li>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/categories', 'CategoryController@index')->name('categories');
 
 Auth::routes();
