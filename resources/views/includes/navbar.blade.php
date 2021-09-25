2036d80a54e8dbfc83e96610522d145218283275
ferri yusra <ferisakuragi@gmail.com>
2021-09-25 13:49:34 +0700

    update navigation after login and before login

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top" data-aos="fade-down">
     <div class="container">
       <a href="{{ route('home')}}" class="navbar-brand">
-        <img src="{{url('/images/logo.svg')}}" alt="Logo">
+        <img src="{{ url('/images/logo.svg')}}" alt="Logo">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
         <span class="navbar-toggler-icon"></span>
@@ -19,21 +19,65 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="categories.html" class="nav-link">
+            <a href="{{ route('home')}}" class="nav-link">
               Rewards
             </a>
           </li>
+          @guest
           <li class="nav-item">
-            <a href="register.html" class="nav-link">
-              Sign Up
+            <a href="{{ route('register')}}" class="nav-link">
+              Daftar
             </a>
           </li>
           <li class="nav-item">
-            <a href="login.html" class="btn btn-success nav-link px-4 text-white">
-              Sign in
+            <a href="{{ route('login')}}" class="btn btn-success nav-link px-4 text-white">
+              Masuk
             </a>
           </li>
+          @endguest
         </ul>
+
+        @auth
+            <!-- desktop menu -->
+            <ul class="navbar-nav d-none d-lg-flex">
+              <li class="nav-item dropdown">
+                  <a href="#" class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown">
+                      <img src="images/icon-user.png" alt="" class="rounded-circle mr-2 profile-picture">
+                      Hi, {{Auth::user()->name}}
+                  </a>
+                  <div class="dropdown-menu">
+                      <a href="{{ route('dashboard')}}" class="dropdown-item">
+                          Dashboard
+                      </a>
+                      <a href="{{route('dashboard-settings-account')}}" class="dropdown-item">
+                          Pengaturan
+                      </a>
+                      <div class="dropdown-divider"></div>
+                      <a href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();"
+                      class="dropdown-item">Keluar</a>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                      </form>
+                  </div>
+              </li>
+              <li class="nav-item">
+                  <a href="#" class="nav-link d-inline-block mt-2">
+                      <img src="images/icon-cart-empty.svg" alt="cart">
+                  </a>
+              </li>
+          </ul>
+          <!-- mobile menu -->
+          <ul class="navbar-nav d-block d-lg-none">
+              <li class="nav-item">
+                  <a href="#" class="nav-link">Hi, Ferri </a>
+              </li>
+              <li class="nav-item">
+                  <a href="#" class="nav-link d-inline-block">Cart</a>
+              </li>
+          </ul>
+        @endauth
       </div>
     </div>
   </nav>
