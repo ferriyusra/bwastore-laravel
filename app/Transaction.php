17164d7e960187033d14bac773b18720d694ecf2
ferri yusra <ferisakuragi@gmail.com>
2021-10-06 18:48:40 +0700

    install and config midtrans-php, create model transaction and transaction-detail

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    
+    protected $fillable = [
+      'users_id',
+      'insurance_price',
+      'shipping_price',
+      'total_price',
+      'code_transaction'
+    ];
+   
+    protected $hidden = [];
 }
---
+++ app/TransactionDetail.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionDetail extends Model
+{
+    protected $fillable = [
+        'transactions_id',
+        'products_id',
+        'price',
+        'shipping_status',
+        'resi',
+        'code_transaction'
+      ];
+     
+    protected $hidden = [];
+}
